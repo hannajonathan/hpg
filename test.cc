687ab13e215b21ac3644f9b481f494e52612fbe8
Martin Pokorny <mpokorny@nrao.edu>
2020-11-02 11:24:42 -0700

    Add set_convolution_function() to test for Cuda

--- test.cc
+++ test.cc
@@ -10,6 +10,8 @@ struct MyCF2
 
   std::vector<std::complex<float>> values;
 
+  MyCF2() {}
+
   MyCF2(
     unsigned oversampling_,
     unsigned size,
@@ -30,19 +32,19 @@ struct MyCF2
 int
 main(int argc, char* argv[]) {
 
-  const unsigned cf2_oversampling = 10;
-  const unsigned cf2_size = 31;
-  const unsigned cf2_num_values =
-    cf2_oversampling * cf2_size * cf2_oversampling * cf2_size;
-  std::vector<std::complex<float>> cf2_values;
-  cf2_values.reserve(cf2_num_values);
+  MyCF2 cf2;
   {
+    const unsigned oversampling = 10;
+    const unsigned size = 31;
+    const unsigned num_values = oversampling * size * oversampling * size;
+    std::vector<std::complex<float>> values;
+    values.reserve(num_values);
     std::mt19937 gen(42);
     std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
-    for (auto i = 0; i < cf2_num_values; ++i)
-      cf2_values.emplace_back(dist(gen), dist(gen));
+    for (auto i = 0; i < num_values; ++i)
+      values.emplace_back(dist(gen), dist(gen));
+    cf2 = MyCF2(oversampling, size, values);
   }
-  MyCF2 cf2(cf2_oversampling, cf2_size, cf2_values);
 
   hpg::initialize();
 #ifdef HPG_ENABLE_SERIAL
@@ -69,11 +71,14 @@ main(int argc, char* argv[]) {
       hpg::GridderState(hpg::Device::Cuda, {1000, 2000, 3}, {0.1, -0.1});
     auto st1 = st0.fence();
     auto st2 = std::move(st0).fence();
+    auto st3 =
+      std::move(st2).set_convolution_function(hpg::Device::OpenMP, cf2);
   }
   {
     auto g0 = hpg::Gridder(hpg::Device::Cuda, {1000, 2000, 3}, {0.1, -0.1});
     g0.fence();
     g0.fence();
+    g0.set_convolution_function(hpg::Device::OpenMP, cf2);
   }
 #endif // HPG_ENABLE_CUDA
   hpg::finalize();
