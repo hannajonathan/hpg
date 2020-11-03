83c52ccccde5007549544267ad17c6c0c86e768e
Martin Pokorny <mpokorny@nrao.edu>
2020-11-03 12:08:29 -0700

    Add call to grid_visibilities() in test

--- test.cc
+++ test.cc
@@ -37,17 +37,66 @@ create_cf2(Generator& gen) {
   const unsigned num_values = oversampling * size[0] * oversampling * size[1];
   std::vector<std::complex<hpg::cf_fp>> values;
   values.reserve(num_values);
-  std::uniform_real_distribution<hpg::cf_fp> dist(-1.0f, 1.0f);
+  std::uniform_real_distribution<hpg::cf_fp> dist(-1.0, 1.0);
   for (auto i = 0; i < num_values; ++i)
     values.emplace_back(dist(gen), dist(gen));
   return MyCF2(oversampling, size, values);
 }
+
+template <typename Generator>
+void
+init_visibilities(
+  Generator& gen,
+  std::vector<std::complex<hpg::visibility_fp>>& visibilities,
+  std::vector<hpg::vis_weight_fp>& visibility_weights,
+  std::vector<hpg::vis_frequency_fp>& visibility_frequencies,
+  std::vector<hpg::vis_phase_fp>& visibility_phase,
+  std::vector<hpg::vis_uvw_t>& visibility_coordinates) {
+
+  const unsigned num_visibilities = 1000000;
+  visibilities.clear();
+  visibilities.reserve(num_visibilities);
+  visibility_weights.clear();
+  visibility_weights.reserve(num_visibilities);
+  visibility_frequencies.clear();
+  visibility_frequencies.reserve(num_visibilities);
+  visibility_phase.clear();
+  visibility_phase.reserve(num_visibilities);
+  visibility_coordinates.clear();
+  visibility_coordinates.reserve(num_visibilities);
+
+  std::uniform_real_distribution<hpg::visibility_fp> dist_vis(-1.0, 1.0);
+  std::uniform_real_distribution<hpg::vis_weight_fp> dist_weight(0.0, 1.0);
+  std::uniform_real_distribution<hpg::vis_uvw_fp> dist_uvw(-475.0, 475.0);
+  for (auto i = 0; i < num_visibilities; ++i) {
+    visibilities.emplace_back(dist_vis(gen), dist_vis(gen));
+    visibility_weights.push_back(dist_weight(gen));
+    visibility_frequencies.push_back(2.92513197327302e9);
+    visibility_phase.emplace_back(0.0);
+    visibility_coordinates.push_back(
+      hpg::vis_uvw_t({dist_uvw(gen), dist_uvw(gen), 0.0}));
+  }
+}
+
 int
 main(int argc, char* argv[]) {
 
   std::mt19937 rng(42);
 
   MyCF2 cf2 = create_cf2(rng);
+
+  std::vector<std::complex<hpg::visibility_fp>> visibilities;
+  std::vector<hpg::vis_weight_fp> visibility_weights;
+  std::vector<hpg::vis_frequency_fp> visibility_frequencies;
+  std::vector<hpg::vis_phase_fp> visibility_phase;
+  std::vector<hpg::vis_uvw_t> visibility_coordinates;
+  init_visibilities(
+    rng,
+    visibilities,
+    visibility_weights,
+    visibility_frequencies,
+    visibility_phase,
+    visibility_coordinates);
 
   hpg::initialize();
 #ifdef HPG_ENABLE_SERIAL
@@ -65,6 +114,13 @@ main(int argc, char* argv[]) {
     g0.fence();
     g0.fence();
     g0.set_convolution_function(hpg::Device::OpenMP, cf2);
+    g0.grid_visibilities(
+      hpg::Device::OpenMP,
+      visibilities,
+      visibility_weights,
+      visibility_frequencies,
+      visibility_phase,
+      visibility_coordinates);
   }
 #endif // HPG_ENABLE_SERIAL
 #ifdef HPG_ENABLE_CUDA
@@ -82,6 +138,13 @@ main(int argc, char* argv[]) {
     g0.fence();
     g0.fence();
     g0.set_convolution_function(hpg::Device::OpenMP, cf2);
+    g0.grid_visibilities(
+      hpg::Device::OpenMP,
+      visibilities,
+      visibility_weights,
+      visibility_frequencies,
+      visibility_phase,
+      visibility_coordinates);
   }
 #endif // HPG_ENABLE_CUDA
   hpg::finalize();
