6559085569b86a06055eb9a380d7dfc9cd3c0f46
Martin Pokorny <mpokorny@nrao.edu>
2021-04-05 10:57:25 -0600

    Add SPDX-License-Identifiers

--- hpgVersionDetails.cmake
+++ hpgVersionDetails.cmake
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# SPDX-License-Identifier: Apache-2.0
+#
 
 # This file should contain nothing but the following line setting the
 # project version. The variable name must not clash with the
--- src/hpg_indexing.hpp
+++ src/hpg_indexing.hpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+// SPDX-License-Identifier: Apache-2.0
+//
 #pragma once
 #include "hpg.hpp"
 #include <array>
