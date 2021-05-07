77fcd919fc75c072d78f6ed729c36aa799421a8d
Martin Pokorny <mpokorny@nrao.edu>
2021-05-07 14:19:21 -0600

    Reformat license statements

--- tests/GridArraysUnitTests.cc
+++ tests/GridArraysUnitTests.cc
@@ -1,4 +1,5 @@
 // Copyright 2021 Associated Universities, Inc. Washington DC, USA.
+// SPDX-License-Identifier: Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,8 +12,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// SPDX-License-Identifier: Apache-2.0
 //
 #define HPG_INTERNAL
 #include "hpg.hpp"
