c752ab31e99c78be18195b3a16cfb9d49753979e
Jonathan <jonathanhanna812@gmail.com>
2024-07-03 16:46:38 -0600

    forgot comma

--- src/hpg_runtime.hpp
+++ src/hpg_runtime.hpp
@@ -1050,7 +1050,7 @@ public:
       model,
       m_grid,
       m_mean_grid,
-      m_weights
+      m_weights,
       m_moment);
       //std::cout << "completed visibilitygridder 2" << std::endl;
     return exec_grid.copy_visibilities_to_host(return_visibilities);
